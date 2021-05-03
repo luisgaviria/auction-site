77d3508034c8e2582c9224f58ef00891203ef5af
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-05-02 21:53:42 -0400

    New edits

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -3,8 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <div className="not-container">
-      <h1>Making it right.</h1>
-      <h2>Bringing you all of the Real Estate auctions in one place so you can focus on buying.</h2>
+      <img
+        className="octopus"
+        src="https://i.postimg.cc/brY38wj3/9ff0c4d9ca9e3d947b2a439095a7d5b4.jpg"
+      />
+      <h1>Bringing you all of the Real Estate auctions in one place so you can focus on buying.</h1>
     </div>
   );
 };
