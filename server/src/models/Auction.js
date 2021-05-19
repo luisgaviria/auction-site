16927922840a388d33140319958afbece7b2c335
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-05-19 16:27:54 -0400

    new edits

---
+++ server/src/models/Auction.js
@@ -0,0 +1,43 @@
+const Model = require("./Model");
+const uniqueFactory = require("objection-unique");
+
+const unique = uniqueFactory({
+  fields: ["address"],
+  identifiers: ["id"],
+});
+
+class Auction extends unique(Model) {
+  static get tableName() {
+    return "auctions";
+  }
+  static get relationMappings() {
+    const { Favorite, User } = require("./index.js");
+    return {
+      favorites: {
+        relation: Model.HasManyRelation,
+        modelClass: Favorite,
+        join: {
+          from: "auctions.id",
+          to: "favorites.auctionId",
+        },
+      },
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: "auctions.userId",
+          to: "users.id",
+        },
+      },
+    };
+  }
+  static get jsonSchema() {
+    return {
+      type: "object",
+      status: { type: "string", minLength: 1, maxLength: 300 },
+      address: { type: "string" },
+    };
+  }
+}
+
+module.exports = Auction;
---
+++ server/src/models/Favorite.js
@@ -0,0 +1,44 @@
+const Model = require("./Model");
+
+class Favorite extends Model {
+  static get tableName() {
+    return "favorites";
+  }
+
+  static get relationMappings() {
+    const Auction = require("./Auction.js");
+    const User = require("./User.js");
+
+    return {
+      park: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Auction,
+        join: {
+          from: "favorites.auctionId",
+          to: "auctions.id",
+        },
+      },
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: "favorites.userId",
+          to: "users.id",
+        },
+      },
+    };
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+
+      properties: {
+        comments: { type: "string", minLength: 1, maxLength: 255 },
+        auctionId: { type: ["string", "integer"] },
+      },
+    };
+  }
+}
+
+module.exports = Favorite;
