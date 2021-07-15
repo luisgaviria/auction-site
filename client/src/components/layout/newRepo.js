ab9d842409878b84e62710edb762c4496bd89496
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-07-15 17:57:54 -0400

    new edits marker infowindow

--- client/src/components/layout/newRepo.js
+++ client/src/components/layout/newRepo.js
@@ -20,62 +20,11 @@ const RepoList = (props) => {
         const error = new Error(errorMessage);
         throw error;
       }
+
+      console.log(response.body);
       const body = await response.json();
 
-      // Geocode.setApiKey("AIzaSyBIa95EK04YAEKm3rg3QN0nbxmRpTRIwk4");
-      // Geocode.setLanguage("en");
-      // Geocode.setRegion("us");
-      //Geocode.enableDebug();
-      const auctions_addresses = [];
-      body.allAuctions.map(async (auction) => {
-        let response2 = await fetch(
-          `https://api.opencagedata.com/geocode/v1/geojson?q=${auction.address}&key=5d72e4941deb43e2ad787f1e9fe5a68b&pretty=1`
-        );
-        response2 = await response2.json();
-        console.log(response2);
-        const location = {
-          lat: response2.features[0].geometry.coordinates[0],
-          lng: response2.features[0].geometry.coordinates[1],
-        };
-
-        sendLocationToDb(location);
-
-        // const response = await Geocode.fromAddress(auction.address);
-        auctions_addresses.push({
-          location: location,
-          address: auction.address,
-        });
-      });
-
-      const sendLocationToDb = async (location) => {
-        try {
-          const response = await fetch(`/api/v1/crawl`, {
-            method: "POST",
-            headers: new Headers({
-              "Content-Type": "application/json",
-            }),
-            body: JSON.stringify(location),
-          });
-          if (!response.ok) {
-            if (response.status === 422) {
-              const body = await response.json();
-              const newErrors = translateServerErrors(body.errors);
-
-              return setErrors(newErrors);
-            } else {
-              const errorMessage = `${response.status} (${response.statusText})`;
-              const error = new Error(errorMessage);
-              throw error;
-            }
-          } else {
-            return;
-          }
-        } catch (error) {
-          console.error(`Error in fetch: ${error.message}`);
-        }
-      };
-
-      setState({ ...state, repo: body.allAuctions, addresses: auctions_addresses });
+      setState({ ...state, repo: body.allAuctions });
     } catch (err) {
       console.error(`Error in fetch: ${err.message}`);
     }
--- server/src/controllers/scrapToDatabase.js
+++ server/src/controllers/scrapToDatabase.js
@@ -11,8 +11,16 @@ import crawlBaystate from "../apiClient/crawlBaystate.js";
 import PatriotCrawl from "../apiClient/PatriotCrawl.js";
 import crawlSullivan from "../apiClient/crawlSullivan.js";
 import crawlJake from "../apiClient/crawlJake.js";
+import NodeGeocoder from "node-geocoder";
 
 import Auction from "../models/Auction.js";
+
+const options = {
+  provider: "google",
+  apiKey: "AIzaSyBIa95EK04YAEKm3rg3QN0nbxmRpTRIwk4",
+};
+
+const geocoder = NodeGeocoder(options);
 
 const scrapToDatabase = async (req, res) => {
   try {
@@ -25,7 +33,7 @@ const scrapToDatabase = async (req, res) => {
       url: "http://www.commonwealthauction.com/auctions.asp?location=1",
     });
 
-    const data2 = await crawlTowne({ url: "https://www3.towneauction.com/Auctions_NoNav.aspx" });
+    // const data2 = await crawlTowne({ url: "https://www3.towneauction.com/Auctions_NoNav.aspx" });
     const data3 = await crawlDean({ url: "http://www.deanassociatesinc.com/auctions.htm" });
     const data4 = await crawlApg({ url: "https://apg-online.com/auction-schedule/" });
     const data5 = await crawlTache({
@@ -64,7 +72,7 @@ const scrapToDatabase = async (req, res) => {
 
     allAuctions = data.concat(
       data1,
-      data2,
+      // data2,
       data3,
       data4,
       data5,
@@ -77,7 +85,7 @@ const scrapToDatabase = async (req, res) => {
       data12
     );
 
-    console.log(allAuctions);
+    // console.log(allAuctions);
 
     let sorted = allAuctions.sort(date_sort_asc).reverse();
 
@@ -93,19 +101,33 @@ const scrapToDatabase = async (req, res) => {
 
     for (let i = 0; i < sorted2.length; i++) {
       try {
-        // console.log(sorted2[i]);
-        const auctionTemp = await Auction.query().insert({
-          deposit: sorted2[i].deposit,
-          link: sorted2[i].link,
-          logo: sorted2[i].logo,
-          state: sorted2[i].state,
-          city: sorted2[i].city,
-          date: sorted2[i].date,
+        let auctionTemp = await Auction.query().findOne({
           address: sorted2[i].address,
-          time: sorted2[i].time,
-          status: sorted2[i].status,
         });
-      } catch (error) {}
+
+        if (!auctionTemp) {
+          const geostuff = await geocoder.geocode(sorted2[i].address);
+          const lat = geostuff[0].latitude.toString();
+          const lng = geostuff[0].longitude.toString();
+          console.log(lat, lng);
+          console.log(sorted2[i].status);
+          const auctionTemp = await Auction.query().insert({
+            deposit: sorted2[i].deposit,
+            link: sorted2[i].link,
+            logo: sorted2[i].logo,
+            state: sorted2[i].state,
+            city: sorted2[i].city,
+            date: sorted2[i].date,
+            address: sorted2[i].address,
+            time: sorted2[i].time,
+            status: sorted2[i].status,
+            lat: lat,
+            lng: lng,
+          });
+        }
+      } catch (error) {
+        // console.log(error);
+      }
     }
 
     sorted2.map((auc) => {
@@ -114,7 +136,7 @@ const scrapToDatabase = async (req, res) => {
 
     const databaseAuctions = await Auction.query();
     sorted2.map(async (auc) => {
-      console.log(auc.address);
+      // console.log(auc.address);
       // if(auc.address.search('Natick')!=-1){
       //   console.log(auc);
       // }
@@ -133,7 +155,7 @@ const scrapToDatabase = async (req, res) => {
 
     return res.status(200).json({ message: "Succesfully updated database", allAuctions: sorted2 });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return res.status(500).json({ errors: error });
   }
 };
