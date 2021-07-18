a6eaba11539e91701e9324fa92bff4c6b59f86c4
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-07-18 02:38:21 -0400

    new edits topbar

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
+import { Navbar, Nav, Form, FormControl, NavDropdown, Button } from "react-bootstrap";
+
 import SignOutButton from "../authentication/SignOutButton";
 
 const TopBar = ({ user }) => {
@@ -8,14 +10,20 @@ const TopBar = ({ user }) => {
     history.push("/favorites");
   };
   const unauthenticatedListItems = [
-    <li key="sign-in">
+    <Button variant="outline-secondary" className="sign-in">
       <Link to="/user-sessions/new">Sign In</Link>
-    </li>,
-    <li key="sign-up">
-      <Link to="/users/new" className="button">
-        Sign Up
-      </Link>
-    </li>,
+    </Button>,
+    // <li key="sign-in" className="button" id="sign-out">
+    //   <Link to="/user-sessions/new">Sign In</Link>
+    // </li>,
+    // <li key="sign-up" className="button" id="sign-out">
+    //   <Link to="/users/new" className="button">
+    //     Sign Up
+    //   </Link>
+    // </li>,
+    <Button className="sign-up" variant="outline-secondary">
+      <Link to="/users/new">Sign Up</Link>
+    </Button>,
   ];
 
   const authenticatedListItems = [
@@ -25,29 +33,73 @@ const TopBar = ({ user }) => {
   ];
 
   return (
-    <div className="main-header">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li>
-            <a type="button" class="button hollow secondary large color black" href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <button
-              type="button"
-              class="button hollow secondary large color black"
-              onClick={onClickHandle}
-            >
-              Favorites
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div className="top-bar-right">
-        <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
-      </div>
-    </div>
+    <header>
+      <Navbar className="navbar" collapseOnSelect expand="md" bg="light" variant="light">
+        <Navbar.Brand>
+          <a href="http://www.urabatv.com" className="navbar-brand">
+            <img
+              className="d-inline-block"
+              src="https://i.postimg.cc/gcw0FY8B/circle-cropped-4-1.png"
+            ></img>
+          </a>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            {/* <Nav.Link className="navbar-brand-text">
+              <Link className="navbar-brand-text" to="/">
+                Home
+              </Link>
+            </Nav.Link>  */}
+            <Nav.Link className="navbar-brand-text">
+              <Link className="navbar-brand-text" to="/">
+                Home
+              </Link>
+            </Nav.Link>
+
+            <Nav.Link className="navbar-brand-text">
+              <Link className="navbar-brand-text" to="/favorites">
+                Favorites
+              </Link>
+            </Nav.Link>
+            <Nav.Link className="navbar-brand-text">
+              <Link className="navbar-brand-text" to="/about">
+                About Us
+              </Link>
+            </Nav.Link>
+          </Nav>
+
+          <Nav.Link className="ml-auto">
+            {user ? authenticatedListItems : unauthenticatedListItems}
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+
+    // return (
+    //   <div className="main-header">
+    //     <div className="top-bar-left">
+    //       <ul className="menu">
+    //         <li>
+    //           <a type="button" class="button hollow secondary large color black" href="/">
+    //             Home
+    //           </a>
+    //         </li>
+    //         <li>
+    //           <button
+    //             type="button"
+    //             class="button hollow secondary large color black"
+    //             onClick={onClickHandle}
+    //           >
+    //             Favorites
+    //           </button>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //     <div className="top-bar-right">
+    //       <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
+    //     </div>
+    //   </div>
   );
 };
 
