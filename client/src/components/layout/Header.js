284441d125b9aa834dee1b9808720e18528fa843
Luis Gaviria <luisgaviria@macbook-pro-2.mynetworksettings.com>
2022-02-24 17:28:22 -0500

    test1

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -4,25 +4,24 @@ import Spinner from "react-bootstrap/Spinner";
 const Header = () => {
   return (
     <>
-      <h1>Auction & Co.</h1>
+      <h1 className="heading-primary">Auction & Co.</h1>
 
-      <React.Suspense
+      {/* <React.Suspense
         fallback={
           <Spinner animation="border" role="status">
             <span className="visually-hidden"></span>
           </Spinner>
         }
       >
-        <div className="not-container" alt="logo, drawing of people attending auction">
-          <img
-            alt="logo-of-site"
-            className="not-container2"
-            src="https://i.postimg.cc/wTjbqjsv/20945153-Convert-Image.jpg"
-          />
-        </div>
-      </React.Suspense>
-
-      <h3>
+          </React.Suspense> */}
+      <div className="not-container" alt="logo, drawing of people attending auction">
+        <img
+          alt="logo-of-site"
+          className="not-container2"
+          src="https://i.postimg.cc/wTjbqjsv/20945153-Convert-Image.jpg"
+        />
+      </div>
+      <h3 className="heading-primary-main">
         Bringing you all of the Massachusetts Real Estate auctions in one place so you can focus on
         buying.
       </h3>
