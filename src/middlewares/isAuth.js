d30b36d2a0208e072d7d9a906a8d609bc9725741
Luis Gaviria <luisgaviria@macbook-pro-2.mynetworksettings.com>
2022-04-27 17:28:33 -0400

    new edit

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -1,30 +1,29 @@
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-export const isAuth = (req,res,next)=>{
-    const authtoken = req.get("Authorization");
-    if(!authtoken){
-        const error = new Error();
-        error.message = "Not authenticated!";
-        error.statusCode = 401;
-        throw error;
-    }
-    const token = authtoken.split(" ")[1];
-    let decodeToken;
-    try{
-        decodeToken = jwt.verify(token,process.env.JWT_SECRET);
-    }
-    catch(err){
-        err.statusCode = 500;
-        throw err;
-    }
-    if(!decodeToken){
-        const error = new Error();
-        error.message = "Not authenticated!";
-        error.statusCode = 401;
-        throw error;
-    }
-    req.userId = decodeToken.id;
-    next();
+export const isAuth = (req, res, next) => {
+  const authtoken = req.get("Authorization");
+  if (!authtoken) {
+    const error = new Error();
+    error.message = "Not authenticated!";
+    error.statusCode = 401;
+    throw error;
+  }
+  const token = authtoken.split(" ")[1];
+  let decodeToken;
+  try {
+    decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  if (!decodeToken) {
+    const error = new Error();
+    error.message = "Not authenticated!";
+    error.statusCode = 401;
+    throw error;
+  }
+  req.userId = decodeToken.id;
+  next();
 };
