8b09a56bba60f36f25d71e2071408a1f2cfe7e14
Patryk404 <patryk744@op.pl>
2023-03-20 21:36:24 +0100

    Fixed Baystate

--- src/apiClient/crawlBaystate.js
+++ src/apiClient/crawlBaystate.js
@@ -2,8 +2,8 @@ import puppeteer from "puppeteer";
 
 const crawl = async ({ url }) => {
   const logo =
-    "";
-  const link = "https://auction-site-ma.herokuapp.com/auction_photos/baystate.webp";
+    "https://auction-site-ma.herokuapp.com/auction_photos/baystate.webp";
+  const link = "https://www.baystateauction.com/auctions";
 
   const browser = await puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] });
   const page = await browser.newPage();
