b7354da587ed126618983167db4565348e8e66d3
Patryk404 <patryk744@op.pl>
2023-06-16 22:53:38 +0200

    Working with api

--- src/controllers/getData.js
+++ src/controllers/getData.js
@@ -1,4 +1,29 @@
 import filter from "../utils/filter.js";
+import axios from "axios"; 
+
+const streetTypeMapping = {
+  al: 'alley',
+  ave: 'avenue',
+  blvd: 'boulevard',
+  cir: 'circle',
+  ct: 'court',
+  dr: 'drive',
+  hwy: 'highway',
+  ln: 'lane',
+  pkwy: 'parkway',
+  pl: 'place',
+  plz: 'plaza',
+  rd: 'road',
+  st: 'street',
+  ter: 'terrace',
+  way: 'way',
+  w: 'west',
+  n: 'north',
+  e: 'east',
+  s: 'south'
+  // Add more mappings as needed
+};
+
 
 const paginate = (array, page_size, page_number) => {
   return array.slice((page_number - 1) * page_size, page_number * page_size);
@@ -34,11 +59,23 @@ const getData = async (req, res) => {
 
   auctions = paginate(auctions,limit,page);
 
-  console.log(auctions);
+  // console.log(auctions);
 
   for (let auction of auctions) {
     auction.date = auction.date.toLocaleDateString();
   }
+
+  for (const auction of auctions){
+    console.log(auction.address)
+
+    // console.log(auction.lat);
+    // console.log(auction.lng);
+    await checkIfItExistsInAPI(auction);
+  }
+  
+  // auctions.map(auction=>{
+  //   checkIfItExistsInAPI(auction);
+  // }); 
 
   return res.status(200).json({
     allAuctions: auctions,
@@ -47,5 +84,32 @@ const getData = async (req, res) => {
     limit: limit,
   });
 };
+
+const checkIfItExistsInAPI =async (auction)=>{ // find an algorithm to actually check if auc addresses from database are match with addresses from api
+  let address = auction.address.toLowerCase();
+  address = address.split(' ');
+  const lat = auction.lat;
+  const lng = auction.lng;
+  try{
+
+    const res = await axios.get(`https://api.bridgedataoutput.com/api/v2/mlspin/listings?access_token=${process.env.SERVER_TOKEN}&near=${lng},${lat}`)
+    const bundle = res.data.bundle; 
+    for (const auction of bundle){
+      let aucApiAddress = auction.UnparsedAddress.toLowerCase(); 
+      aucApiAddress = aucApiAddress.split(' ');
+      // console.log(aucApiAddress);
+    }
+    // console.log(res.data.bundle[0].UnparsedAddress);
+    // console.log(res.data.bundle[0].UnparsedAddress);
+  }
+  catch(err){
+    // console.log(err);
+  }
+
+};
+
+// const getDataFromExternalAPI =async (req,res)=>{ 
+
+// };
 
 export default getData;
