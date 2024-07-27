38bfe4cde5be8761a5389dcefa2cc28a9d546632
Patryk404 <patryk744@op.pl>
2024-07-27 23:56:06 +0200

    feat: updated scrapping script

--- src/apiClient/scrap.js
+++ src/apiClient/scrap.js
@@ -1,4 +1,12 @@
 import scrapToDatabase from "../controllers/scrapToDatabase.js";
+import knex from "knex";
+import objection from "objection";
+import knexConfig from "../../knexfile.cjs";
+
+const knexConnection = knex(knexConfig);
+
+objection.Model.knex(knexConnection);
+
 
 // const knex = Knex(knexConfig);
 // Model.knex(knex);
