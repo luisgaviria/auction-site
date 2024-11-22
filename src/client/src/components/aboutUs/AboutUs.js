bebdca038cd5028d2c7669a3ac04c16fc88743ef
luisgaviria <luis.aptx@gmail.com>
2024-11-22 09:04:31 -0500

    remove sign up

--- src/client/src/components/aboutUs/AboutUs.js
+++ src/client/src/components/aboutUs/AboutUs.js
@@ -5,29 +5,37 @@ const AboutUs = () => {
     <div>
       <br />
       <p className="about-us-text">
-        This website was originally created as a personal tool to save time on
-        the Real Estate auction purchases.
+        This website was originally created as a personal project to save time
+        on real estate auction purchases.
       </p>
       <p className="about-us-text">
-        Let’s face it. Currently it's very time consuming to search for Real
-        Estate auctions on multiple websites everyday.
+        Let’s face it. Currently, it's very time-consuming to search for real
+        estate auctions on multiple websites every day.
       </p>
       <p className="about-us-text">
-        Most Auctioneer websites in the Massachusetts area are slow, dated, not
-        easy to use and hard to find on your own and this makes the process very
-        inefficient. Countless hours are wasted searching for auctions that
-        often get cancelled or postponed making it even more difficult for the
-        Real Estate investors to keep up.
+        Most auctioneer websites in the Massachusetts area are slow, dated, not
+        easy to use, and hard to find. This makes the process very inefficient.
+        Countless hours are wasted searching for auctions that often get
+        canceled or postponed, making it even more difficult for real estate
+        investors to keep up.
       </p>
       <p className="about-us-text">
-        At Auction & Company, we believe your time deserves better and that's
-        why we created this website which in the long run will save you
-        countless hours and make the buying process more enjoyable.
+        At Auction & Company, we believe your time deserves better. That’s why
+        we created this website to save you countless hours and make the buying
+        process more efficient and enjoyable.
       </p>
       <p className="about-us-text">
-        Today, Auction & Company gets data from over 10 Auctioneer websites in
-        the state of Massachusetts which are then displayed for you on one site
-        sorted by the soonest auctions happening every day.
+        Please note, Auction & Company is not an auctioneer. While we strive to
+        provide accurate and up-to-date information, this is a personal project
+        offered as a free service. As such, it is not maintained to the highest
+        professional standards. Users are responsible for conducting their own
+        due diligence and confirming that auctions are taking place as
+        scheduled.
+      </p>
+      <p className="about-us-text">
+        Today, Auction & Company gathers data from over 10 auctioneer websites
+        in Massachusetts and displays it in one place, conveniently sorted by
+        the soonest auction dates.
       </p>
     </div>
   );
